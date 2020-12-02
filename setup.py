eb3e6c1cf7cd9fdfebbae1f3e6e30ed96ae1d5a7
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-12-02 09:35:29 +0100

    chore: refactoring developer tooling
    - using black (closes #28)
    - add release please (closes #26)
    - add issue and PR template (closes #25)
    - preparing docs

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Perform sanity checks on MOF structures"""
 import sys
 
 from setuptools import find_packages, setup
@@ -6,55 +7,49 @@ from setuptools import find_packages, setup
 import versioneer
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
-try:
-    with open('README.md', 'r') as handle:
-        long_description = handle.read()
-except:
-    long_description = ''
+with open("README.md", "r") as handle:
+    LONG_DESCRIPTION = handle.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
     # Self-descriptive entries which should always be present
-    name='mofchecker',
-    author='Kevin M. Jablonka',
-    author_email='kevin.jablonka@epfl.ch',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    name="mofchecker",
+    author="Kevin M. Jablonka",
+    author_email="kevin.jablonka@epfl.ch",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    license='MIT',
+    license="MIT",
     packages=find_packages(),
     include_package_data=True,
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=pytest_runner,
     install_requires=requirements,
     extras_require={
-        'docs': ['guzzle_sphinx_theme'],
-        'pre-commit': [
-            'pre-commit',
-            'yapf',
-            'prospector',
-            'pylint',
-            'versioneer',
-            'isort',
+        "testing": ["pytest", "pytest-cov<2.11"],
+        "docs": [
+            "sphinx",
+            "sphinx-book-theme",
+            "sphinx-autodoc-typehints",
+            "sphinx-copybutton",
         ],
-        'changelog': ['gitchangelog']
     },
     entry_points={
-        'console_scripts': [
-            'mofchecker = mofchecker.cli:main',
+        "console_scripts": [
+            "mofchecker = mofchecker.cli:main",
         ],
     },
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
