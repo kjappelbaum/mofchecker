1ba95fe606ebb784abfdb5f6079c8eebf1a22fe7
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-10-01 13:27:02 +0200

    Bump version: 0.9.5 → 0.9.6-dev

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "mofchecker"
 copyright = "2022, Kevin M. Jablonka"
 author = "Kevin M. Jablonka"
-release = "0.9.5"
+release = "0.9.6-dev"
 
 
 # -- General configuration ---------------------------------------------------
